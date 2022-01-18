e553cf16f8f17e459755d1eb5103dde999a1efda
Henrique Morikawa <henriqueyukiharu@gmail.com>
2022-01-17 22:10:27 -0300

    create TransactionTable component

---
+++ src/components/TransactionTable/TransactionsTable.component.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./TransactionsTable.styles";
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="withdraw">R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>17/01/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">R$1.100,00</td>
+            <td>Casa</td>
+            <td>17/01/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
